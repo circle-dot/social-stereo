ce2550bc28f8f36cdf4f351be262606e0dc1c4b4
MarazzaM <marazza.matias@gmail.com>
2024-10-23 16:40:10 +0700

    Remove frontend from DO for now

--- update.sh
+++ update.sh
@@ -17,11 +17,11 @@ fi
 
 # Build and restart the Docker containers from the app directory (~/myapp)
 echo "Rebuilding and restarting Docker containers..."
-sudo docker-compose down
-sudo docker-compose up --build -d
+sudo docker-compose -f dev.docker-compose.yml down
+sudo docker-compose -f dev.docker-compose.yml up --build -d
 
 # Check if Docker Compose started correctly
-if ! sudo docker-compose ps | grep "Up"; then
+if ! sudo docker-compose -f dev.docker-compose.yml ps | grep "Up"; then
   echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
   exit 1
 fi
