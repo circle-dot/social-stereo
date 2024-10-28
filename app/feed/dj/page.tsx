c1ffea8901da865cb9e15f7c326e1d27c9a559e2
MarazzaM <marazza.matias@gmail.com>
2024-10-29 00:44:05 +0700

    Add coming soon

--- app/feed/dj/page.tsx
+++ app/feed/dj/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className="bg-custom-darkGreen rounded-xl p-4 flex flex-col space-y-2">
+      <h2 className="text-lg font-semibold text-white">Coming Soon</h2>
+      </div>
   )
 }
 
