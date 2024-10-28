0f502a4816e8140bc0352a1367b65b1e8ac3a964
MarazzaM <marazza.matias@gmail.com>
2024-10-29 00:39:23 +0700

    Add funct to button to actually go back

--- components/ui/TopNavigation.tsx
+++ components/ui/TopNavigation.tsx
@@ -5,11 +5,12 @@ import { ArrowLeft } from 'lucide-react'
 function TopNavigation() {
   return (
     <div className="w-full flex justify-between items-center p-4">
-
         <div>
-            <ArrowLeft className='text-custom-lightGreen'/>
+            <ArrowLeft 
+                className='text-custom-lightGreen cursor-pointer' 
+                onClick={() => window.history.back()}
+            />
         </div>
-
     </div>
   )
 }
