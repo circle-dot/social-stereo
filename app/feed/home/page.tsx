c35a7c67a8d8899e0f28e0cea6cd33fca3c6ef49
MarazzaM <marazza.matias@gmail.com>
2024-10-30 16:22:07 +0700

    Update copy

--- app/feed/home/page.tsx
+++ app/feed/home/page.tsx
@@ -4,46 +4,48 @@ import { SITE_CONFIG } from '@/config/site';
 const cardData = [
   {
     icon: <Headphones />,
-    title: "Music",
-    description: "Lorem ipsum dolor sit amet consectetur. Loboris orci malesuada nunc lobortis turpis proin lectus nibh.",
+    title: "Decentralized Playlist",
+    description: "Vote for the music you like to contribute to the playlist.",
     href: "/feed/music"
   },
   {
     icon: <Music />,
     title: "Decentralized DJs",
-    description: "Lorem ipsum dolor sit amet consectetur. Loboris orci malesuada nunc lobortis turpis proin lectus nibh.",
+    description: "Vouch for your favorite DJs.",
     href: "/feed/dj"
   },
   {
     icon: <Mic />,
     title: "Karaoke",
-    description: "Lorem ipsum dolor sit amet consectetur. Loboris orci malesuada nunc lobortis turpis proin lectus nibh.",
+    description: "Vouch for friends, so they can cut the line and have their moment of fame.",
     href: "/feed/karaoke"
   },
   {
     icon: <Gift />,
-    title: "Special Box",
-    description: "Lorem ipsum dolor sit amet consectetur. Loboris orci malesuada nunc lobortis turpis proin lectus nibh.",
+    title: "DevCon Pack",
+    description: "Coming soon...",
     href: "/feed/special-box"
   }
 ];
 
 function Page() {
   return (
-    <div className=" mx-auto space-y-4 p-4 bg-custom-purple h-full pb-24">
-      <div>
-      <h1 className='!font-extrabold text-2xl md:text-3xl lg:text-4xl pb-2'>{SITE_CONFIG.description}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vero quidem consequuntur hic nihil perferendis.</p>
+    <div className="mx-auto space-y-4 p-4 bg-custom-purple h-full pb-24">
+      <div className="max-w-4xl">
+        <h1 className='!font-extrabold text-2xl md:text-3xl lg:text-4xl pb-2'>{SITE_CONFIG.description}</h1>
+        <p>Select a category to start</p>
       </div>
-      {cardData.map((card, index) => (
-        <NavigationCard
-          key={index}
-          icon={card.icon}
-          title={card.title}
-          description={card.description}
-          href={card.href}
-        />
-      ))}
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl">
+        {cardData.map((card, index) => (
+          <NavigationCard
+            key={index}
+            icon={card.icon}
+            title={card.title}
+            description={card.description}
+            href={card.href}
+          />
+        ))}
+      </div>
     </div>
   );
 }
