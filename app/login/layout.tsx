e24b8b949d91ff574891eda1cefae9bb49d57ee4
MarazzaM <marazza.matias@gmail.com>
2024-11-01 20:20:39 +0700

    Update login routing

---
+++ app/login/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import PalmTreeBackground from "@/components/ui/music/PalmTreeBackground";
+
+export const metadata: Metadata = {
+  title: "Social Stereo",
+  description: "Social Stereo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <PalmTreeBackground>
+      <div className="min-h-screen flex flex-col">
+        <main className="flex-grow ">
+          {children}
+        </main>
+      </div>
+    </PalmTreeBackground>
+  );
+}
