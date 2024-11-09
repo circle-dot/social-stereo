e1b88abafbf3e722b461800c6745d14568adc56f
MarazzaM <marazza.matias@gmail.com>
2024-11-09 17:25:16 +0700

    Remove any later

--- components/devcon/stamps/StampCollection.tsx
+++ components/devcon/stamps/StampCollection.tsx
@@ -1,5 +1,6 @@
-'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
+'use client';
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 import { useEffect, useState } from "react"
