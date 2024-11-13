36a81af70c21700924bfcaa268a998721f789f27
MarazzaM <marazza.matias@gmail.com>
2024-11-13 13:56:02 +0700

    Change test for loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black z-50">
+    <div className="fixed inset-0 flex justify-center items-center bg-[#3E1F48] z-50">
       <Image
-        src="/goldcurator.png"
+        src="/stamps/goldcurator.png"
         alt="Loading"
-        width={100}
-        height={100}
-        className="animate-pulse"
+        width={200}
+        height={200}
+        className="animate-pulse rounded-full"
       />
     </div>
   );
