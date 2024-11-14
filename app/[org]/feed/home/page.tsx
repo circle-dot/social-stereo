c8e7db6b2f535ff4c0f41e7e6fa5b9d9837f05f3
MarazzaM <marazza.matias@gmail.com>
2024-11-14 13:04:08 +0700

    Add button to hear top 20

--- app/[org]/feed/home/page.tsx
+++ app/[org]/feed/home/page.tsx
@@ -1,4 +1,4 @@
-import { Headphones, MoveRight } from 'lucide-react';
+import { Headphones, MoveRight, Music } from 'lucide-react';
 import StampCollection from '@/components/devcon/stamps/StampCollection';
 import Link from 'next/link';
 
@@ -24,20 +24,38 @@ export default function HomePage({ params }: PageProps) {
         <StampCollection />
       </div>
       
-      <Link 
-        href={`/${params.org}/feed/music`}
-        prefetch={true}
-        className="bg-custom-darkGreen rounded-xl p-4 flex flex-col space-y-2 max-w-lg hover:bg-custom-darkGreen/90 transition-colors duration-200"
-      >
-        <div className="text-2xl text-custom-lightGreen">
-          <Headphones />
-        </div>
-        <h2 className="text-lg font-semibold text-white">Decentralized Playlist</h2>
-        <p className="text-xs text-gray-300">Vote for the music you like to contribute to the playlist.</p>
-        <div className="mt-4 py-2 px-8 rounded-full gap-3 inline-flex items-center justify-center bg-custom-lightGreen text-black text-base md:text-lg hover:bg-custom-lightGreen/50 hover:text-custom-white transition-colors duration-200">
-          Play <MoveRight className='w-3 h-3 ml-2' />
-        </div>
-      </Link>
+      <div className="flex flex-col gap-4">
+        <Link 
+          href={`/${params.org}/feed/music`}
+          prefetch={true}
+          className="bg-custom-darkGreen rounded-xl p-4 flex flex-col space-y-2 max-w-lg hover:bg-custom-darkGreen/90 transition-colors duration-200"
+        >
+          <div className="text-2xl text-custom-lightGreen">
+            <Headphones />
+          </div>
+          <h2 className="text-lg font-semibold text-white">Decentralized Playlist</h2>
+          <p className="text-xs text-gray-300">Vote for the music you like to contribute to the playlist.</p>
+          <div className="py-2 px-8 rounded-full gap-3 inline-flex items-center justify-center bg-custom-lightGreen text-black text-base md:text-lg hover:bg-custom-lightGreen/50 hover:text-custom-white transition-colors duration-200">
+            Curate playlist <MoveRight className='w-3 h-3 ml-2' />
+          </div>
+        </Link>
+
+        <a 
+          href="https://open.spotify.com/playlist/557Ufv5mviacUVDrfT8l07?si=QObSWgiASDK_JW9El-F3gA"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-custom-darkGreen rounded-xl p-4 flex flex-col space-y-2 max-w-lg hover:bg-custom-darkGreen/90 transition-colors duration-200"
+        >
+          <div className="text-2xl text-custom-lightGreen">
+            <Music />
+          </div>
+          <h2 className="text-lg font-semibold text-white">Listen on Spotify</h2>
+          <p className="text-xs text-gray-300">Check out the current top 20 tracks on Spotify.</p>
+          <div className="py-2 px-8 rounded-full gap-3 inline-flex items-center justify-center bg-custom-lightGreen text-black text-base md:text-lg hover:bg-custom-lightGreen/50 hover:text-custom-white transition-colors duration-200">
+            Play top 20 <MoveRight className='w-3 h-3 ml-2' />
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
